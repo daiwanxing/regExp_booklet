9f0dafc0e090e10daf0361d4f0446d6c0df81899
daiwanxing <wanxing.dai@coollu.com.cn>
2022-08-24 10:08:13 +0800

    update util.js

--- utils.js
+++ utils.js
@@ -22,3 +22,21 @@ function escapeHTML (str) {
 }
 
 escapeHTML("<p>This A Normal paragraph</p>")
+
+
+/**
+ * @description 将等值的实体转换成HTML字符串
+ * @param { string } str
+ */
+function unEscapeHTML (str) {
+    const htmlEntity = {
+        lt: "<",
+        gt: ">",
+        quot: '"',
+        amp: "&",
+    }
+
+    return str.replace(/\&([^;]+);/g, function(match, key) {
+        return htmlEntity[key] || match;
+    })
+}
