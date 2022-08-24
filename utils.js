0a244711c2872b3af0528babedc9f227eeba6d12
daiwanxing <wanxing.dai@coollu.com.cn>
2022-08-24 18:18:40 +0800

    add regExp-03.md

--- utils.js
+++ utils.js
@@ -37,7 +37,7 @@ function unEscapeHTML (str) {
     }
 
     return str.replace(/\&([^;]+);/g, function(match, key) {
-        // 如果有分组引用，则key为分组匹配的字符， 否则第二个参数是匹配的串的索引
+        // 如果有分组引用，则key为分组匹配的字符， 否则第二个参数是匹配的串的第一个字符出现的索引
         console.log(match, key); 
         return htmlEntity[key] || match;
     })
